b07eb22b9002e39f4e802099f6fd73f825f6caf9
soveshtripathy15-code <sovesh.tripathy15@gmail.com>
2026-04-23 08:57:49 +0530

    Fix syntax error in print statement

--- hellosovesh.java
+++ hellosovesh.java
@@ -2,7 +2,7 @@ public class hellosovesh
 {
     public static void main(String[] args) 
     {
-         System.out.println(good morning")
+         System.out.println("good morning")
         System.out.println("welcome to vikas java programme");
         
     }
